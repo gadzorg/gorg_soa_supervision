d9f73552ee8eb66d582f92929e74e2a72d0571c3
Colin Darie <colin@darie.eu>
2021-02-22 15:25:41 +0100

    Bundle: replace factory_girl by factory_bot

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,8 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :event do
-    event_name "MyString"
-    published_at "2016-09-05 15:53:11"
-    sender_id "MyString"
-    data ""
+    event_name { "MyString" }
+    published_at { "2016-09-05 15:53:11" }
+    sender_id { "MyString" }
+    data { "" }
   end
 end
